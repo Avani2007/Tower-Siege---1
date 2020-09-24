499af279af96ea4176b69202eff1bd9accb054f5
Avani2007 <69082362+Avani2007@users.noreply.github.com>
2020-09-24 18:28:58 +0530

    Add files via upload

---
+++ pyramid.js
@@ -0,0 +1,20 @@
+class Blocks{
+  constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':0.5
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      push();
+      fill("silver")
+      rectMode(CENTER)
+      rect(this.body.position.x, this.body.position.y,this.width,this.height)
+      pop();
+    }
+}
